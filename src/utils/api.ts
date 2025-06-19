5fa07a31d40322d2937e5686de61b69324eef0c2
PedroPiresy <pedro.pires@debarry.cloud>
2025-06-19 15:12:18 -0300

    UPDATE: Atualiza a URL base da API para utilizar a variável de ambiente VITE_API_BASE_URL.

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
-//const API_BASE_URL = 'http://localhost:5134'; // Ajuste para sua URL da API
-const API_BASE_URL = 'https://palavrimapi.onrender.com';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 let palavraAtual: string = '';
 
 export const api = {
